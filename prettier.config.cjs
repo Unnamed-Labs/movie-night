d938031a6f131ecb4fba643f68cc323abd8f3aff
Aaron Mathews <aamathews23@gmail.com>
2023-04-06 22:05:07 -0700

    fix lint

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,12 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 100,
+  endOfLine: 'auto',
+  singleAttributePerLine: true,
 };
 
 module.exports = config;
