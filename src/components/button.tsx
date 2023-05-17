ab7e46243582b0b6aabe935a79a5880ff729d6df
Riley Golden <riley9460@gmail.com>
2023-05-17 01:16:42 -0700

    added standalone and refined var classes

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,14 +1,8 @@
 import React, { type ReactNode } from 'react';
 
-/* interface LinkProps {
-  children: ReactNode;
-  variant?: 'page1' | 'page2' | 'page3' | 'page4' | 'page5' | 'page6' | 'page7';
-  className?: string;
-} */
-
 interface ButtonProps {
   children: ReactNode;
-  variant?: 'primary' | 'secondary' | 'disabled';
+  variant?: 'primary' | 'secondary' | 'disabled' | 'standalone';
   className?: string;
   onClick?: () => void;
   disabled?: boolean;
@@ -25,15 +19,17 @@ const Button: React.FC<ButtonProps> = ({
   switch (variant) {
     case 'primary':
       buttonType =
-        'bg-c flex basis-2 flex-col rounded bg-emerald-300 px-20 py-3 text-xl text-slate-700 duration-150 hover:bg-emerald-600 hover:text-white';
+        'bg-c flex rounded bg-emerald-300 px-20 py-3 text-xl text-slate-700 duration-150 hover:bg-emerald-600 hover:text-white';
       break;
     case 'secondary':
       buttonType =
-        'bg-c flex basis-2 flex-col rounded bg-purple-300 px-20 py-3 text-xl text-slate-700 hover:bg-purple-500 hover:text-white';
+        'bg-c flex rounded bg-purple-300 px-20 py-3 text-xl text-slate-700 hover:bg-purple-500 hover:text-white';
       break;
     case 'disabled':
-      buttonType =
-        'bg-c flex basis-2 flex-col rounded  bg-slate-500 px-20 py-3 text-xl text-slate-700';
+      buttonType = 'bg-c flex rounded bg-slate-500 px-20 py-3 text-xl text-slate-700';
+      break;
+    case 'standalone':
+      buttonType = 'bg-c flex basis-4 py-3 text-xl text-white underline hover:no-underline';
       break;
   }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,10 +33,11 @@ const Home: NextPage = () => {
           >
             Ready
           </Button>
+          <Button variant="standalone">Cancel</Button>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="src\pages\button.tsx"
+              href="https://create.t3.gg/en/usage/first-steps"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">First Steps →</h3>
