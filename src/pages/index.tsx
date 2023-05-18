170bc64a4404be1f75e1b2798065360aef1c7898
Aaron Mathews <aamathews23@gmail.com>
2023-05-17 23:16:12 -0700

    added basic cypress test rel: #3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            Movie Night
           </h1>
           <Button variant="primary">Ready</Button>;<Button variant="secondary">Ready</Button>;
           <Button
