ce0078173f3a1e87c24304ca38944a1b327e28d2
Aaron Mathews <aamathews23@gmail.com>
2023-06-01 21:27:08 -0700

    added button unit tests rel: #3

--- src/components/global/Button.tsx
+++ src/components/global/Button.tsx
@@ -2,43 +2,41 @@ import React, { type ReactNode } from 'react';
 
 interface ButtonProps {
   children: ReactNode;
-  variant?: 'primary' | 'secondary' | 'disabled' | 'standalone';
-  className?: string;
-  onClick?: () => void;
+  variant?: 'primary' | 'secondary' | 'standalone';
   disabled?: boolean;
+  onClick?: () => void;
 }
 
 const Button: React.FC<ButtonProps> = ({
   children,
   variant = 'primary',
-  onClick,
   disabled = false,
+  onClick,
 }) => {
-  let buttonType = '';
+  let buttonType =
+    'flex rounded min-w-[144px] justify-center px-4 items-center py-3 text-xl text-slate-500';
 
-  switch (variant) {
-    case 'primary':
-      buttonType =
-        'flex rounded bg-emerald-300 min-w-[144px] justify-center px-4 items-center py-3 text-xl text-slate-700 transition-colors hover:bg-emerald-600 hover:text-white';
-      break;
-    case 'secondary':
-      buttonType =
-        'flex rounded bg-purple-300 min-w-[144px] justify-center px-4 items-center py-3 text-xl text-slate-700 transition-colors hover:bg-purple-500 hover:text-white';
-      break;
-    case 'disabled':
-      buttonType =
-        'flex rounded bg-slate-500 min-w-[144px] justify-center px-4 items-center py-3 text-xl text-slate-700';
-      break;
-    case 'standalone':
-      buttonType = 'flex basis-4 py-3 text-xl text-white underline hover:no-underline';
-      break;
+  if (disabled) {
+    buttonType += ' bg-slate-700 hover:cursor-not-allowed';
+  } else {
+    switch (variant) {
+      case 'primary':
+        buttonType += ' bg-emerald-300 transition-colors hover:bg-emerald-600 hover:text-white';
+        break;
+      case 'secondary':
+        buttonType += ' bg-purple-300 transition-colors hover:bg-purple-500 hover:text-white';
+        break;
+      case 'standalone':
+        buttonType = 'flex basis-4 py-3 text-xl text-white underline hover:no-underline';
+        break;
+    }
   }
 
   return (
     <button
-      className={`${buttonType}`}
-      onClick={onClick}
+      className={buttonType}
       disabled={disabled}
+      onClick={onClick}
     >
       {children}
     </button>
