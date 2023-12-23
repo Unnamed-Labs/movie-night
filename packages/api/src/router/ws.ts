5e20d7df4abb3404851f3f7af04d7cdf54762675
Aaron Mathews <aamathews23@gmail.com>
2023-12-22 17:03:59 -0800

    progress

--- packages/api/src/router/ws.ts
+++ packages/api/src/router/ws.ts
@@ -1,8 +1,6 @@
 import { createTRPCRouter } from '../trpc';
-import { randomNumber } from './ws/randomNumber';
 import { lobby } from './ws/lobby';
 
 export const wsRouter = createTRPCRouter({
-  randomNumber,
   lobbyWs: lobby,
 });
