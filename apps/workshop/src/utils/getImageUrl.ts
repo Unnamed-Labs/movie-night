fce0195e5965472554c140aa5dcc4f2b0d079399
Aaron Mathews <aamathews23@gmail.com>
2024-01-20 16:56:58 -0800

    cleanup

--- apps/workshop/src/utils/getImageUrl.ts
+++ apps/workshop/src/utils/getImageUrl.ts
@@ -2,8 +2,6 @@ export const getImageUrl = (src: string) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
   const workshopUrl = import.meta.env.STORYBOOK_URL;
 
-  console.log(workshopUrl);
-
   if (workshopUrl) {
     return `${workshopUrl}${src}`;
   }
