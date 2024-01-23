d0f364b7f32e59cd2057624d4f04059d1bb29129
Aaron Mathews <aamathews23@gmail.com>
2024-01-22 16:35:26 -0800

    fix movie card and page centering rel: #78

--- apps/game/src/components/Page.tsx
+++ apps/game/src/components/Page.tsx
@@ -7,7 +7,7 @@ type PageProps = {
 };
 
 export const Page = ({ title, body, loading, error, children }: PageProps) => (
-  <section className="flex min-h-screen flex-col gap-8 bg-slate-900 p-8 text-slate-50">
+  <section className="flex min-h-screen flex-col items-center gap-8 bg-slate-900 p-8 text-slate-50">
     <h1 className="font-pacifico text-center text-5xl">{title}</h1>
     {body && <p className="font-raleway text-center text-base">{body}</p>}
     {loading ? <div>{loading}</div> : error ? <div>{error}</div> : <>{children}</>}
